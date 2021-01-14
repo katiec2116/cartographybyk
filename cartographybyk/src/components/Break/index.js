5d96a0e4b49457fd89c81057d07f8febb60ae43c
katiec <katiecampbell21@outlook.com>
2021-01-14 17:57:59 -0500

    added MDBootstrap and updated jumbotron

---
+++ cartographybyk/src/components/Break/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Break() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Break
--- cartographybyk/src/pages/Home/index.js
+++ cartographybyk/src/pages/Home/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import background from './KC.jpg';
+import React from 'react';
+import Jumbotron from '../../components/Jumbotron';
 
 
 function Home() {
     return (
-        <div className="container p-0 m-0">
-            <div class="row">
-                <div class="col text-center"></div>
-                <img className="background" src={background} />
-                <h1 className="display-1 text-center" id="logo">Cartography By K</h1>
-            </div>
+        <div>
+            <Jumbotron/>
+            
         </div>
     )
 }
